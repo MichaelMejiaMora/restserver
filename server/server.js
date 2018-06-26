73c17e906c0770236ae179097971fb77725fa2f7
Michael Mejia <mejia.mora@gmail.com>
2018-06-25 20:49:23 -0600

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,42 @@
+require('../config/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', (req, res) => {
+  res.json('GET Usuario')
+})
+
+app.post('/usuario', (req, res) => {
+  let body = req.body
+  if(body.name === undefined) {
+    res.status(400).json({
+      ok: false,
+      mensaje: 'El nombre es necesario'
+    })
+  } else {
+    res.json({
+      persona: body
+    })
+  }
+})
+
+app.put('/usuario/:id', (req, res) => {
+  id = req.params.id
+  res.json({
+    id
+  })
+})
+
+app.delete('/usuario', (req, res) => {
+  res.json('Delete Usuario')
+})
+
+app.listen(process.env.PORT, () => console.log(`Escuchando en el puerto ${process.env.PORT}`))
